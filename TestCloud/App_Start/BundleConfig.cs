99269aec3831eb421517a2090abf1de046cac219
maurov <maurov@DESKTOP-GQ1IN3L>
2020-02-21 00:07:07 -0300

    Se ordena el codigo

--- TestCloud/App_Start/BundleConfig.cs
+++ TestCloud/App_Start/BundleConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System.Configuration;
+using System.Web;
 using System.Web.Optimization;
 
 namespace TestCloud
@@ -8,23 +9,33 @@ namespace TestCloud
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            var environment = ConfigurationManager.AppSettings["environment"];
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            if (environment == "debug")
+            {
+                bundles.Add(new ScriptBundle("~/bundles/core").Include(
+                        "~/Scripts/AngularJs/myApp.js",
+                        "~/Scripts/AngularJs/portfolioController.js",
+                        "~/Scripts/AngularJs/portfolioCardsController.js",
+                        "~/Scripts/AngularJs/contactController.js",
+                        "~/Scripts/general.js",
+                        "~/Scripts/index.js",
+                        "~/Scripts/cookieHandler.js",
+                        "~/Scripts/portfolio.js"));
+            }
+            else
+            {
+                // TODO: para produccion, minificar los controllers y hacer un bundle del resto
+                bundles.Add(new ScriptBundle("~/bundles/core").Include(
+                        "~/Scripts/AngularJs/myApp.js",
+                        "~/Scripts/AngularJs/portfolioController.js",
+                        "~/Scripts/AngularJs/portfolioCardsController.js",
+                        "~/Scripts/AngularJs/contactController.js",
+                        "~/Scripts/general.js",
+                        "~/Scripts/index.js",
+                        "~/Scripts/cookieHandler.js",
+                        "~/Scripts/portfolio.js"));
+            }                    
         }
     }
 }
